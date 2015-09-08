210fa23345a34389d8363fad53622fbb39f69ac4
punk86862011 <punk86862011@gmail.com>
2015-09-07 23:33:16 -0700

    Change to my email.

--- layout/php/send.php
+++ layout/php/send.php
@@ -2,7 +2,7 @@
 
 /////////// Add your own email below //////////////// 
 
-	define("WEBMASTER_EMAIL", 'bitpublimedia@gmail.com');
+	define("WEBMASTER_EMAIL", 'punk86862011@gmail.com');
 	
 	error_reporting (E_ALL ^ E_NOTICE);
 
